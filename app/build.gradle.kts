b54ec20baf81ce44348432faeaf75004165e60be
flash159483 <taecinjung@gmail.com>
2023-06-29 20:38:46 +0900

    [chore] gradle / link app gradle to common.gradle (#1)

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("android")
+    id("com.android.application")
+//    id("dagger.hilt.android.plugin")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.lighthouse.lingo_swap"
+
+    defaultConfig {
+        applicationId = "com.lighthouse.lingo_swap"
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
