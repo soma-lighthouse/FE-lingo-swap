8a0b8e5b73b9a8c0e9f29fa95ec32c79293d2a43
flash159483 <taecinjung@gmail.com>
2023-06-29 20:52:22 +0900

    [chore] module / modified common-ui gradle file (#1)

--- common-ui/build.gradle.kts
+++ common-ui/build.gradle.kts
@@ -5,35 +5,21 @@ plugins {
 
 android {
     namespace = "com.lighthouse.android.common_ui"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 28
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+    api(project(":domain"))
+    implementation(project(":shared"))
 
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
