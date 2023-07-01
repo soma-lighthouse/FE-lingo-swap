17a4eb1c724ca7325978db80d28810508a472770
flash159483 <taecinjung@gmail.com>
2023-07-01 18:54:04 +0900

    [chore] module / change dependencies (#2)

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -7,3 +7,10 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
+
+dependencies {
+    implementation(project(":entity"))
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("javax.inject:javax.inject:1")
+}
--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("android")
     id("com.android.library")
+    id("dagger.hilt.android.plugin")
     kotlin("kapt")
 }
 
@@ -16,14 +17,26 @@ android {
             )
         }
     }
+
+
     buildFeatures {
         dataBinding = true
     }
 }
 
+//kapt {
+//    javacOptions {
+//        // These options are normally set automatically via the Hilt Gradle plugin, but we
+//        // set them manually to workaround a bug in the Kotlin 1.5.20
+//        option("-Adagger.fastInit=ENABLED")
+//        option("-Adagger.hilt.android.internal.disableAndroidSuperclassValidation=true")
+//    }
+//}
+
 dependencies {
     implementation(project(":common-ui"))
 
+    implementation("androidx.activity:activity-ktx:1.7.2")
     implementation(libs.bundles.androidx.ui.foundation)
     implementation(libs.material)
     implementation(libs.constraintlayout)
