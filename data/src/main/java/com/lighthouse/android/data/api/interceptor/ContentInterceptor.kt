feaa5a1a2d84fde192620029bb188123f74fa239
flash159483 <taecinjung@gmail.com>
2023-07-21 17:21:51 +0900

    [chore] added okHttp interceptor client (#5)

---
+++ data/src/main/java/com/lighthouse/android/data/api/interceptor/ContentInterceptor.kt
@@ -0,0 +1,32 @@
+package com.lighthouse.android.data.api.interceptor
+
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import okhttp3.Interceptor
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
+
+object ContentInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+        return try {
+            val responseData = response.body?.string()
+            val responseJson = convertStringToJsonObject(responseData)
+            val contentsArray = getContentsList(responseJson)
+            val contentsString = contentsArray.toString()
+            response.newBuilder().body(contentsString.toResponseBody()).build()
+        } catch (e: Exception) {
+            response
+        }
+    }
+
+    private fun convertStringToJsonObject(jsonString: String?): JsonObject {
+        return JsonParser.parseString(jsonString).asJsonObject
+    }
+
+    private fun getContentsList(json: JsonObject): JsonArray {
+        return json.getAsJsonArray("contents")
+    }
+}
