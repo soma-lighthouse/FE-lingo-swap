8a5b20f376f19357d6caa79575ce1b8790c43831
flash159483 <seungwonjung0313@gmail.com>
2023-07-31 20:41:25 +0900

    [LH-46] Add Navigation Module #8

---
+++ navigation/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    namespace = "com.lighthouse.navigation"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation(libs.bundles.androidx.ui.foundation)
+    implementation(libs.material)
+    implementation(libs.bundles.navigation)
+}
