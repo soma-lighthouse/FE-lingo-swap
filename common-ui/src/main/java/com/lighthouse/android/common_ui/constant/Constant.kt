54fa9380fd6cf5436d3fad287e35ebee75f72775
flash159483 <seungwonjung0313@gmail.com>
2023-08-07 13:25:20 +0900

    [LH-112] Add resource for Board #16

--- common-ui/src/main/java/com/lighthouse/android/common_ui/constant/Constant.kt
+++ common-ui/src/main/java/com/lighthouse/android/common_ui/constant/Constant.kt
@@ -1,7 +1,7 @@
 package com.lighthouse.android.common_ui.constant
 
 object Constant {
-    const val PROFILE_IMAGE_SIZE = 80.0f
+    const val PROFILE_IMAGE_SIZE = 64.0f
     const val PROFILE_FLAG_SIZE = 25.0f
     const val PAGE_SIZE = 10
 }
