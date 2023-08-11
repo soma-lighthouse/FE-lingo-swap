dcf8c5de3ee12105b1e39febf8b1ab6ca036d11e
flash159483 <seungwonjung0313@gmail.com>
2023-08-11 11:20:04 +0900

    [LH-139] Add sendbird and firebase dependencies #18

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.safe.args) apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
 
 subprojects {
