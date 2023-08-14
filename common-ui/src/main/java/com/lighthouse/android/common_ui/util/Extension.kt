e568e45f2dd5879fdba2ae5a4a7685c95f8894d8
flash159483 <seungwonjung0313@gmail.com>
2023-08-14 21:12:14 +0900

    [LH-149] Add valid detection and keyboard handler #19

--- common-ui/src/main/java/com/lighthouse/android/common_ui/util/Extension.kt
+++ common-ui/src/main/java/com/lighthouse/android/common_ui/util/Extension.kt
@@ -5,10 +5,20 @@ import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
 import android.content.res.Resources
+import android.os.Build
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.util.Patterns
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.core.os.bundleOf
+import java.io.Serializable
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 fun View.setVisible() {
     this.visibility = View.VISIBLE
@@ -51,4 +61,59 @@ inline fun <reified T : Activity> Context.navigateActivity(
     vararg argument: Pair<String, Any?>,
 ) {
     startActivity(buildIntent<T>(*argument))
+}
+
+fun <T : Serializable> Intent.intentSerializable(key: String, clazz: Class<T>): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        this.getSerializableExtra(key, clazz)
+    } else {
+        this.getSerializableExtra(key) as T?
+    }
+}
+
+fun String.setColor(color: Int): SpannableString {
+    val result = SpannableString(this)
+
+    result.setSpan(
+        ForegroundColorSpan(color),
+        0,
+        this.length,
+        SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    return result
+}
+
+fun String?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+
+fun String?.isValidBirthday(): Boolean {
+    if (isNullOrEmpty()) {
+        return false
+    }
+
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA)
+    dateFormat.isLenient = false
+
+    return try {
+        val date = dateFormat.parse(this)
+
+        val minDate = Calendar.getInstance()
+        minDate.add(Calendar.YEAR, -120)
+        val maxDate = Calendar.getInstance()
+        maxDate.add(Calendar.YEAR, -1)
+
+        date != null && date.after(minDate.time) && date.before(maxDate.time)
+    } catch (e: ParseException) {
+        // Parsing error, input is not in valid format
+        false
+    }
+}
+
+fun EditText.onCloseKeyBoard(context: Context) {
+    this.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+        if (!hasFocus) {
+            val inputMethodManager =
+                context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager?
+            inputMethodManager!!.hideSoftInputFromWindow(this.windowToken, 0)
+        }
+    }
 }
