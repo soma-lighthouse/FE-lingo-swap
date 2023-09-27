5e4dc49223c57e9513fc8109117bf38eaec0ae20
flash159483 <seungwonjung0313@gmail.com>
2023-09-27 10:20:10 +0900

    [LH-198] Back handler for detail profile #27

--- feature/profile/src/main/java/com/lighthouse/profile/view/DetailProfileActivity.kt
+++ feature/profile/src/main/java/com/lighthouse/profile/view/DetailProfileActivity.kt
@@ -1,8 +1,11 @@
 package com.lighthouse.profile.view
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
+import androidx.navigation.fragment.NavHostFragment
 import com.lighthouse.android.common_ui.base.BindingActivity
+import com.lighthouse.android.common_ui.dialog.showOKDialog
 import com.lighthouse.profile.R
 import com.lighthouse.profile.databinding.ActivityDetailProfileBinding
 import com.lighthouse.profile.viewmodel.ProfileViewModel
@@ -14,7 +17,38 @@ class DetailProfileActivity :
     private val viewModel: ProfileViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         viewModel.userId = intent.getStringExtra("userId") ?: ""
         viewModel.isMe = intent.getBooleanExtra("isMe", false)
+
+        handleBackPressed()
+    }
+
+    private fun handleBackPressed() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (viewModel.editMode) {
+                    showOKDialog(
+                        this@DetailProfileActivity,
+                        getString(com.lighthouse.android.common_ui.R.string.exit_title),
+                        getString(com.lighthouse.android.common_ui.R.string.exit_body)
+                    ) { _, _ ->
+                        finish()
+                    }
+                } else {
+                    if (navController.currentDestination?.id == R.id.detailFragment) {
+                        finish()
+                    } else {
+                        navController.popBackStack()
+                    }
+                }
+            }
+        }
+
+        this.onBackPressedDispatcher.addCallback(this, callback)
     }
 }
